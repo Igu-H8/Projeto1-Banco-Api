5318d9517859283827a0a56456d2d8645f9b8d9f
Igu-H8 <igorhfsilva@outlook.com>
2025-02-19 14:59:24 -0300

    segundo commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Projeto1-Banco-Api
 Objetivo do projeto: Criar um CRUD usando fastapi e pydantic para administrar um banco do tipo postgreesql com o sql alchemy
+
+
+Nota da versão 1.1: Criei 2 classes para o projeto e 1 função para cadastrar (o CREATE do CRUDE), o postgreSQL não quer
+funcionar na minha maquina :/, vou ter que adiar a criação do banco
--- main.py
+++ main.py
@@ -1,12 +1,54 @@
-import uvicorn
-from fastapi import FastAPI
+# Versão 1.1
+
+from uvicorn import run
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+
+class Empresa(BaseModel):
+    id: int
+    name: str
+    cnpj: int
+    endereco: str
+    email: str
+    telefone: int
+
+
+class Acessoria(BaseModel):
+    id: int
+    name: str
+    periodicidade: str
+    empresas_id: int
+
+
+# Temporario
+empresa = []
+acessoria = []
+# ----------
+
+# empresa
+@app.post("/cad_empresa/", response_model=Empresa)
+def create_item(empresas: Empresa):
+    
+    if any(existing_empresas.id == empresas.id for existing_empresas in empresa):
+        raise HTTPException(status_code=400, detail="ID já existe")
+    
+    if any(existing_empresas.cnpj == empresas.cnpj for existing_empresas in empresa):
+        raise HTTPException(status_code=400, detail="CNPJ já existe")
+    
+    empresa.append(empresas)
+    return empresas
+
+
+
+# MENSSGAEN DE TESTE
 @app.get("/")
 def teste():
     return "A api esta no ar!"
-
+# ------------------
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+    run(app, port=8000)
